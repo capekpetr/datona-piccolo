e1ec253d7ba4931601fa5235e197b4f2bdb3a53b
Petr Čapek <petrcapek24@gmail.com>
2024-08-01 15:56:31 +0200

    Radio buttons finish

--- home/scripts/koupit.js
+++ home/scripts/koupit.js
@@ -3,6 +3,10 @@ const SwitchMonthly = document.querySelector("input#monthly");
 const ClassicPrice = document.querySelector("p#classic-price");
 const ProPrice = document.querySelector("p#pro-price");
 const PlusPrice = document.querySelector("p#plus-price");
+const LicenceFormInputs = document.querySelectorAll("#plans-table .form-input");
+const HardwareFormInputs = document.querySelectorAll(
+  "#hardware .hardware-form-input"
+);
 
 SwitchMonthly.addEventListener("change", function () {
   ClassicPrice.innerText = "170 Kč";
@@ -14,4 +18,30 @@ SwitchYearly.addEventListener("change", function () {
   ClassicPrice.innerText = "135 Kč";
   ProPrice.innerText = "390 Kč";
   PlusPrice.innerText = "200 Kč";
+});
+
+LicenceFormInputs.forEach((input) => {
+  input.addEventListener("change", function () {
+    LicenceFormInputs.forEach((input) => {
+      input.nextElementSibling.firstChild.classList.remove("secondary-button");
+      input.nextElementSibling.firstChild.classList.add("primary-button");
+      input.nextElementSibling.firstChild.innerText = "Vybrat";
+    });
+    this.nextElementSibling.firstChild.classList.remove("primary-button");
+    this.nextElementSibling.firstChild.classList.add("secondary-button");
+    this.nextElementSibling.firstChild.innerText = "Vybráno";
+  });
+});
+
+HardwareFormInputs.forEach((input) => {
+  input.addEventListener("change", function () {
+    HardwareFormInputs.forEach((input) => {
+      input.nextElementSibling.firstChild.classList.remove("secondary-button");
+      input.nextElementSibling.firstChild.classList.add("primary-button");
+      input.nextElementSibling.firstChild.innerText = "Vybrat";
+    });
+    this.nextElementSibling.firstChild.classList.remove("primary-button");
+    this.nextElementSibling.firstChild.classList.add("secondary-button");
+    this.nextElementSibling.firstChild.innerText = "Vybráno";
+  });
 });
