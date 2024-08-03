eb52b7b9e8c4f7adf9eed19046eb3d754fefe528
Petr Čapek <petrcapek24@gmail.com>
2024-08-03 16:49:23 +0200

    First Total Price Calculation Concept

--- home/scripts/koupit.js
+++ home/scripts/koupit.js
@@ -1,12 +1,25 @@
-const SwitchYearly = document.querySelector(" input#yearly");
-const SwitchMonthly = document.querySelector("input#monthly");
-const ClassicPrice = document.querySelector("p#classic-price");
-const ProPrice = document.querySelector("p#pro-price");
-const PlusPrice = document.querySelector("p#plus-price");
-const RadioButtons = function (InputsClass) {
-  document.querySelectorAll(InputsClass).forEach((input) => {
+const switchYearly = document.querySelector(" input#yearly");
+const switchMonthly = document.querySelector("input#monthly");
+const classicPrice = document.querySelector("p#classic-price");
+const proPrice = document.querySelector("p#pro-price");
+const plusPrice = document.querySelector("p#plus-price");
+const classicPriceInput = document.querySelector("input#piccolo-classic");
+const proPriceInput = document.querySelector("input#piccolo-pro");
+const plusPriceInput = document.querySelector("input#piccolo-plus");
+
+function updateLicencePrices(classic, pro, plus) {
+  classicPrice.innerText = `${classic} Kč`;
+  classicPriceInput.dataset.price = classic;
+  proPrice.innerText = `${pro} Kč`;
+  proPriceInput.dataset.price = pro;
+  plusPrice.innerText = `${plus} Kč`;
+  plusPriceInput.dataset.price = plus;
+}
+
+function animateButtons(inputsLocationClass) {
+  document.querySelectorAll(inputsLocationClass).forEach((input) => {
     input.addEventListener("change", function () {
-      document.querySelectorAll(InputsClass).forEach((input) => {
+      document.querySelectorAll(inputsLocationClass).forEach((input) => {
         input.nextElementSibling.firstChild.classList.remove(
           "secondary-button"
         );
@@ -18,19 +31,37 @@ const RadioButtons = function (InputsClass) {
       this.nextElementSibling.firstChild.innerText = "Vybráno";
     });
   });
-};
+}
 
-SwitchMonthly.addEventListener("change", function () {
-  ClassicPrice.innerText = "170 Kč";
-  ProPrice.innerText = "490 Kč";
-  PlusPrice.innerText = "250 Kč";
+function calculateTotalPrice() {
+  const orderQuantity = document.querySelector(
+    "#calculation #order-quantity"
+  ).value;
+  const licencePrice = document.querySelector("#plans-table input:checked")
+    .dataset.price;
+  const hardwarePrice = document.querySelector("#hardware input:checked")
+    .dataset.price;
+  document.querySelector(".calculation-field #licence-price").innerText =
+    licencePrice * orderQuantity;
+  document.querySelector(".calculation-field #hardware-price").innerText =
+    hardwarePrice * orderQuantity;
+  document.querySelector(".calculation-field #total-no-tax").innerText =
+    (+licencePrice + +hardwarePrice) * orderQuantity;
+  document.querySelector(".calculation-field #total-tax").innerText =
+    (+licencePrice + +hardwarePrice) * orderQuantity * 1.21;
+}
+
+switchMonthly.addEventListener("change", function () {
+  updateLicencePrices(170, 490, 250);
 });
 
-SwitchYearly.addEventListener("change", function () {
-  ClassicPrice.innerText = "135 Kč";
-  ProPrice.innerText = "390 Kč";
-  PlusPrice.innerText = "200 Kč";
+switchYearly.addEventListener("change", function () {
+  updateLicencePrices(135, 390, 200);
 });
 
-RadioButtons("#plans-table .form-input");
-RadioButtons("#hardware .hardware-form-input");
+document.addEventListener("change", function () {
+  calculateTotalPrice();
+});
+
+animateButtons("#plans-table .form-input");
+animateButtons("#hardware .hardware-form-input");
